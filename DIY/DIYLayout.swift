03c1c1027fb1cd16554297dce31eecad267699f0
MikhailSkiyAtWork <mvaluyskiy@weezlabs.com>
2016-03-21 11:38:16 +0300

    Added depth into background and foreground

--- DIY/DIYLayout.swift
+++ DIY/DIYLayout.swift
@@ -7,11 +7,37 @@
 //
 
 import UIKit
+
+
+class DIYLayoutAttributes: UICollectionViewLayoutAttributes {
+    
+    var deltaY: CGFloat = 0
+    
+    override func copyWithZone(zone: NSZone) -> AnyObject {
+        let copy = super.copyWithZone(zone) as! DIYLayoutAttributes
+        copy.deltaY = deltaY
+        return copy
+    }
+    
+    override func isEqual(object: AnyObject?) -> Bool {
+        if let attributes = object as? DIYLayoutAttributes {
+            if attributes.deltaY == deltaY{
+                return super.isEqual(object)
+            }
+        }
+        return false
+    }
+}
+
 class DIYLayout : UICollectionViewFlowLayout {
+    
+    override class func layoutAttributesClass() -> AnyClass {
+        return DIYLayoutAttributes.self
+    }
     
     override func layoutAttributesForElementsInRect(rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
         let insets = collectionView!.contentInset
-        let layoutAttributes = super.layoutAttributesForElementsInRect(rect) as [UICollectionViewLayoutAttributes]!
+        let layoutAttributes = super.layoutAttributesForElementsInRect(rect) as! [DIYLayoutAttributes]!
         let offset = collectionView!.contentOffset
         
         let minY = -insets.top
@@ -24,6 +50,7 @@ class DIYLayout : UICollectionViewFlowLayout {
                         frame.size.height = max(minY, headerReferenceSize.height + deltaY)
                         frame.origin.y = CGRectGetMinY(frame) - deltaY
                         attributes.frame = frame
+                        attributes.deltaY = deltaY
                     }
                 }
                 
--- DIY/ScheduleHeaderView.swift
+++ DIY/ScheduleHeaderView.swift
@@ -11,6 +11,25 @@ import UIKit
 class ScheduleHeaderView: UICollectionReusableView {
   
   @IBOutlet private weak var backgroundImageView: UIView!
+  @IBOutlet private weak var backgroundImageViewHeightLayoutConstraint: NSLayoutConstraint!
   @IBOutlet private weak var foregroundImageView: UIView!
+  @IBOutlet private weak var foregroundImageViewHeightLayoutConstraint: NSLayoutConstraint!
+    
+    private var foregroundImageViewHeight: CGFloat = 0
+
+    private var backgroundImageViewHeight: CGFloat = 0
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundImageViewHeight = CGRectGetHeight(backgroundImageView.bounds)
+        foregroundImageViewHeight = CGRectGetHeight(foregroundImageView.bounds)
+    }
+    
+    override func applyLayoutAttributes(layoutAttributes: UICollectionViewLayoutAttributes) {
+        super.applyLayoutAttributes(layoutAttributes)
+        let attributes = layoutAttributes as! DIYLayoutAttributes
+        backgroundImageViewHeightLayoutConstraint.constant = backgroundImageViewHeight - attributes.deltaY
+        foregroundImageViewHeightLayoutConstraint.constant = foregroundImageViewHeight + attributes.deltaY
+    }
   
 }
